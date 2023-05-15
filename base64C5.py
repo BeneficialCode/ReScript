7d5715953f3880810f57097103173fd412efadee
BeneficialCode <3123132899@qq.com>
2023-05-15 10:57:42 +0800

    custom base64

--- base64C5.py
+++ base64C5.py
@@ -11,8 +11,15 @@ def decrypt(text):
     for i in range(0,len(text)):
         key += chr(text[i]-ord(encrypt_key[i%14]))
 
-
     return key
+
+def c1_2016():
+    encoded = 'x2dtJEOmyjacxDemx2eczT5cVS9fVUGvWTuZWjuexjRqy24rV29q'
+    custom_b64 = 'ZYXABCDEFGHIJKLMNOPQRSTUVWzyxabcdefghijklmnopqrstuvw0123456789+/'
+    std_b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    encoded = encoded.translate(str.maketrans(custom_b64,std_b64))
+    flag = base64.b64decode(encoded)
+    print(flag)
 
 def main():
     key = 'UDYs1D7bNmdE1o3g5ms1V6RrYCVvODJF1DpxKTxAJ9xuZW=='.swapcase()
@@ -20,6 +27,7 @@ def main():
     key_decoded = base64.b64decode(key)
     key_decrypted = decrypt(key_decoded)
     print("[+] KEY: {}".format(key_decrypted))
+    c1_2016()
 
 if __name__ == '__main__':
     main()
